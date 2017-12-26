b58c73f3b6d9b7701031b427e10fa4f1dcd8b23b
J. David Ibáñez <jdavid.ibp@gmail.com>
2017-12-26 13:46:05 +0100

    Add query script

---
+++ query.py
@@ -0,0 +1,62 @@
+import datetime
+import os
+import pprint
+import requests
+
+URL = 'http://hycamp.org/wsn/api/query/'
+#URL = 'http://localhost:8000/wsn/api/query/'
+
+def query(limit=100, offset=0, xbee=None, mote=None, sensor=None,
+          tst__gte=None, tst__lte=None, received__gte=None, received__lte=None,
+          debug=False):
+
+    # Paramters
+    if tst__gte:
+        tst__gte = tst__gte.strftime('%Y-%m-%dT%H:%M:%S+00:00')
+    if tst__lte:
+        tst__lte = tst__gte.strftime('%Y-%m-%dT%H:%M:%S+00:00')
+
+    params = {
+        'limit': limit,
+        'offset': offset,
+        'mote': mote,
+        'xbee': xbee,
+        'sensor': sensor,
+        'tst__gte': tst__gte,
+        'tst__lte': tst__lte,
+    }
+
+    # Query
+    headers = {'Authorization': 'Token %s' % TOKEN}
+    response = requests.get(URL, headers=headers, params=params)
+    response.raise_for_status()
+    json = response.json()
+
+    # Debug
+    if debug:
+        pprint.pprint(params)
+        pprint.pprint(json)
+        print()
+
+    return json
+    
+
+if __name__ == '__main__':
+    TOKEN = os.getenv('WSN_TOKEN')
+    if not TOKEN:
+        print("Define the WSN_TOKEN environment variable.")
+    else:
+        response = query(
+            limit=5,
+            mote=161398434909148276,
+            tst__gte=datetime.datetime(2017, 12, 1),
+            debug=True,
+        )
+
+        response = query(
+            limit=5,
+            xbee=5526146534160749,
+            sensor='rssi',
+            received__gte=datetime.datetime(2017, 12, 1),
+            debug=True,
+        )
