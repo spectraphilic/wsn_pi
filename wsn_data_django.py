c1e9f33df67ddc6f64a08e596561c20fcf0827fe
J. David Ibáñez <jdavid.ibp@gmail.com>
2018-07-31 11:52:13 +0200

    Sync waspmote.py from wsn_server

--- wsn_data_django.py
+++ wsn_data_django.py
@@ -1,29 +1,7 @@
-# Standard Library
-from datetime import datetime, timezone
-
 import requests
 
 from mq import MQ
-
-
-def data_to_json(data):
-    """
-    Adapt the data to the structure expected by Django.
-    """
-    # Tags
-    tags = {}
-    for key in 'source_addr_long', 'serial', 'name':
-        value = data.pop(key, None)
-        if value is not None:
-            tags[key] = value
-
-    # Time
-    time = data.pop('tst', None)
-    if time is None:
-        time = data['received']
-    time = datetime.fromtimestamp(time, timezone.utc).isoformat()
-
-    return {'tags': tags, 'frames': [{'time': time, 'data': data}]}
+import waspmote
 
 
 class Consumer(MQ):
@@ -39,7 +17,7 @@ class Consumer(MQ):
         return ('wsn_data', 'fanout', self.name, self.handle_message)
 
     def handle_message(self, data):
-        json = data_to_json(data)
+        json = waspmote.data_to_json(data)
         response = requests.post(self.url, json=json, headers=self.headers)
         status = response.status_code
         assert status == 201, '{} {}'.format(status, response.json())
