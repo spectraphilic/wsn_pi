f563204dbbe9ccc605dcd1b9a7bbee9840fdeb95
pi <pi@raspberrypi>
2019-03-04 12:13:06 +0100

    Improve connection to Django server
    - Use sessions to speed up connection
    - Use timeouts to reduce the likelyhood to break connection with Rabbit
    
    Issue #3

--- wsn_data_django.py
+++ wsn_data_django.py
@@ -12,13 +12,20 @@ class Consumer(MQ):
         super().__init__()
         self.url = self.config['url']
         self.headers = {'Authorization': 'Token %s' % self.config['token']}
+        self.session = requests.Session()
 
     def sub_to(self):
         return ('wsn_data', 'fanout', self.name, self.handle_message)
 
     def handle_message(self, data):
+        # RabbitMQ timeouts after 60s, so we must finish in less than 60s or
+        # the connection to RabbitMQ will be broken.
         json = waspmote.data_to_json(data)
-        response = requests.post(self.url, json=json, headers=self.headers)
+        # 5s to connect. And 30s between reception of bytes; XXX this does not
+        # guarantees that we'll finish below 60s, so there's room for
+        # improvement.
+        timeout = (5, 30)
+        response = self.session.post(self.url, json=json, headers=self.headers, timeout=timeout)
         status = response.status_code
         assert status == 201, '{} {}'.format(status, response.json())
 
