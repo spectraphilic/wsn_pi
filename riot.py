e0ed441a6c7b3f855ac417a0abb14847e3643dfd
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-08-18 17:54:27 +0200

    Riot AS7341 & MLX90614

--- riot.py
+++ riot.py
@@ -8,7 +8,12 @@ SENSORS = {
     2: [('name', 0)],
     3: [('frame', 0)],
     210: [('bme_tc', -2), ('bme_hum', -2), ('bme_pres', 2)],
+    211: [('mlx_object', -2), ('mlx_ambient', -2)],
     219: [('sht_tc', -2), ('sht_hum', -2)],
+    220: [('channel_f1', 0), ('channel_f2', 0), ('channel_f3', 0),
+          ('channel_f4', 0), ('channel_f5', 0), ('channel_f6', 0),
+          ('channel_f7', 0), ('channel_f8', 0), ('channel_clear', 0),
+          ('channel_nir', 0)],
 }
 
 
@@ -30,7 +35,7 @@ def parse_frame(data):
 
 
 if __name__ == '__main__':
-    data = '9f001a611ceaa2011b004b12002e1540190260030018d219095819158d19037d18db1909a61916fdff'
+    data = '9f001a611d2a3b011b004b12002e1540190260030318dc0405080a186c18480c0d131518d219091c19182419037b18d319095f19096318db19096c19196fff'
     data = bytes.fromhex(data)
     frame = parse_frame(data)
     pprint.pprint(frame)
