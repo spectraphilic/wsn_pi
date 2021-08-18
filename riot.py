ec8e3787f7b426d6773717bffff37a244120bb8d
J. David Ibáñez <jdavid.ibp@gmail.com>
2021-08-18 12:38:29 +0200

    New RIOT frame format, more compact

--- riot.py
+++ riot.py
@@ -1,25 +1,34 @@
 import cbor2
+import pprint
 
 
 SENSORS = {
-    0: ('tst', 0),
-    1: ('serial', 0),
-    2: ('name', 0),
-    3: ('frame', 0),
-    130: ('bme_tc', -2),
-    131: ('bme_hum', -2),
-    137: ('bme_pres', 2),
+    0: [('tst', 0)],
+    1: [('serial', 0)],
+    2: [('name', 0)],
+    3: [('frame', 0)],
+    210: [('bme_tc', -2), ('bme_hum', -2), ('bme_pres', 2)],
 }
 
 
 def parse_frame(data):
     data = cbor2.loads(data)
+    n = len(data)
 
     frame = {}
-    for i in range(0, len(data), 2):
+    i = 0
+    while i < n:
         key = data[i]
-        value = data[i+1]
-        name, scale = SENSORS[key]
-        frame[name] = value * (10 ** scale)
+        i += 1
+        for name, scale in SENSORS[key]:
+            value = data[i]
+            i += 1
+            frame[name] = value * (10 ** scale)
 
     return frame
+
+
+if __name__ == '__main__':
+    data = bytes.fromhex('9f001a611cd999011b004b12002e1540190260030018d21908ab1918be19037eff')
+    frame = parse_frame(data)
+    pprint.pprint(frame)
